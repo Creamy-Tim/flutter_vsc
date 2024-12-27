db38c9f8b88d8afa2c962b62cf078fbd8336eb95
leedongha <lee1307dong@gmail.com>
2024-12-27 10:49:17 +0900

    first

---
+++ vscode_app/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Login());
+}
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'test',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Text('로그인'),
+            Text('계정을 입력하거나 소셜 계정으로 로그인 하세요')
+          ],
+        ),
+      ),
+    );
+  }
+}
