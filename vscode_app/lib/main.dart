162643a341b074926ab0d9beb94f3155abdf1f3b
leedongha <lee1307dong@gmail.com>
2024-12-31 11:32:10 +0900

    Add - 초기 화면 추가
    '시작하기' 버튼 기능 구현현
    버튼 디자인 구현 필요
    버튼 외 디자인 구현 필요
    
    Add - 로그인 화면 인터페이스 완성
    
    아이콘 커스텀 아이콘으로 변경 필요
    
    Add - 로그인 화면 '새 계정 생성' 버튼 기능 구현현
    
    Add - 새 계정 생성 화면 추가
    
    Modify - 로그인 화면 피드백 반영
    
    Sizebox -> margin 변경
    가운데정렬 일괄 처리
    커스텀 아이콘으로 변경 필요

--- vscode_app/lib/main.dart
+++ vscode_app/lib/main.dart
@@ -1,372 +1,75 @@
-
 import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+import 'screens/new_account_screen.dart';
 
 void main() {
-  runApp(Login());
+  runApp(MyApp());
 }
 
-class Login extends StatelessWidget {
-  const Login({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: '/FirstScreen',
+      routes: {
+        '/FirstScreen': (context) => const FirstScreen(),
+        '/Login': (context) => Login(),
+        '/NewAccountScreen': (context) => NewAccountScreen(),
+      },
       title: 'Login',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Align(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            SizedBox(height: 82,),
-            Align(
-              alignment: Alignment.center,
-              child: Text("로그인",
-              style: TextStyle(
-                fontWeight: FontWeight.w700,
-                color: Theme.of(context).colorScheme.onBackground,
-                fontSize: 20,
-                fontFamily: 'Pretendard-Bold'),
+            Container(
+              margin: EdgeInsets.only(top: 696),
+              width: 328,
+              height: 54,
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+                border: Border.all(
+                  width: 1.5,
+                ),
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Login(),
+                    ),
+                  );
+                },
+                child: Center(
+                  child: Text(
+                    "시작하기",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      color: Theme.of(context).colorScheme.onPrimary,
+                      fontSize: 14,
+                      fontFamily: 'Pretendard-bold',
+                    ),
+                  ),
+                )
               ),
             ),
-
-
-            SizedBox(height: 10,),
-            Text("계정을 입력하거나 소셜 계정으로 로그인 하세요",
-                style: TextStyle(
-                fontWeight: FontWeight.w400,
-                color: Theme.of(context).colorScheme.onBackground,
-                fontSize: 14,
-                fontFamily: 'Pretendard-Bold'),
-            ),
-
-
-            SizedBox(height: 44,),
-
-
-            SizedBox(
-              width: 320,
-              height: 52,
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: "Enter your Email*",
-                  hintStyle: TextStyle(
-                     color: Theme.of(context).colorScheme.onBackground.withOpacity(0.7),
-                  ),
-                  prefixIcon: Padding(
-                    padding: EdgeInsets.only(left: 22, right: 12),
-                    child: Image.asset(
-                      'assets/img/Email.png',
-                      width: 24,
-                      height: 24,
-                      color: Theme.of(context).colorScheme.onBackground.withOpacity(0.7),
-                    ),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                    borderSide: BorderSide(
-                      color: Theme.of(context).colorScheme.onBackground.withOpacity(0.7),
-                      width: 1.5,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-
-
-            SizedBox(height: 14,),
-            
-
-            SizedBox(
-              width: 320,
-              height: 52,
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: "Password*",
-                  hintStyle: TextStyle(
-                     color: Theme.of(context).colorScheme.onBackground.withOpacity(0.7),
-                  ),
-                  prefixIcon: Padding(
-                    padding: EdgeInsets.only(left: 22, right: 12),
-                    child: Image.asset(
-                      'assets/img/Password.png',
-                      width: 24,
-                      height: 24,
-                      color: Theme.of(context).colorScheme.onBackground,
-                    ),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                    borderSide: BorderSide(
-                      color: Theme.of(context).colorScheme.onBackground.withOpacity(0.7),
-                      width: 1.5,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-
-
-          SizedBox(height: 18,),
-
-
-          SizedBox(
-            width: 302,
-            height: 34,
-            child: Text(
-              "계속하기를 클릭하면 이용 약관과 개인정보 처리방침에 동의하게 됩니다.",
-                style: TextStyle(
-                fontWeight: FontWeight.w200,
-                color: Theme.of(context).colorScheme.onBackground.withOpacity(0.7),
-                fontSize: 12,
-                fontFamily: 'Pretendard-Light'),
-            ),
-          ),
-          
-          
-          SizedBox(height: 38,),
-
-          
-          Container(
-            alignment: Alignment.center,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                  child: Divider(
-                    color: Theme.of(context).colorScheme.onBackground.withOpacity(0.7),
-                    thickness: 1,
-                    indent: 20,
-                    endIndent: 10,
-                  ),
-                ),
-                
-                // 중앙 텍스트
-                Text(
-                  "Or Sign In With",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w200,
-                    color: Theme.of(context).colorScheme.onBackground,
-                    fontSize: 12,
-                    fontFamily: 'Pretendard-Light',
-                  ),
-                ),
-
-                // 오른쪽 Divider
-                Expanded(
-                  child: Divider(
-                    color: Theme.of(context).colorScheme.onBackground.withOpacity(0.7),
-                    thickness: 1,
-                    indent: 10,
-                    endIndent: 20,
-                  ),
-                ),
-              ],
-            ),
-          ),
-
-
-          SizedBox(height: 19,),
-
-
-          Container(
-            alignment: Alignment.center,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                GestureDetector(
-                  onTap: () {
-                    //누르면 실행될 동작
-                  },
-                  child: Container(
-                    width: 95,
-                    height: 52,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Theme.of(context).colorScheme.onBackground.withOpacity(0.7),
-                        width: 1.5,
-                      ),
-                      borderRadius: BorderRadius.circular(30.0),
-                    ),
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-                          child: Image.asset(
-                            'assets/img/google.png',
-                            width: 32,
-                            height: 32,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              
-                SizedBox(width: 17,),
-
-                GestureDetector(
-                  onTap: () {
-                    //누르면 실행될 동작
-                  },
-                  child: Container(
-                    width: 95,
-                    height: 52,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Theme.of(context).colorScheme.onBackground.withOpacity(0.7),
-                        width: 1.5,
-                      ),
-                      borderRadius: BorderRadius.circular(30.0),
-                    ),
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-                          child: Image.asset(
-                            'assets/img/facebook.png',
-                            width: 32,
-                            height: 32,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-
-                SizedBox(width: 17,),
-
-                GestureDetector(
-                  onTap: () {
-                    //누르면 실행될 동작
-                  },
-                  child: Container(
-                    width: 95,
-                    height: 52,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Theme.of(context).colorScheme.onBackground.withOpacity(0.7),
-                        width: 1.5,
-                      ),
-                      borderRadius: BorderRadius.circular(30.0),
-                    ),
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-                          child: Image.asset(
-                            'assets/img/kakao_talk.png',
-                            width: 32,
-                            height: 32,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                
-          
-          
-              ],
-            ),
-          ),
-
-
-          SizedBox(height: 154,),
-
-
-          Container(
-            alignment: Alignment.center,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                GestureDetector(
-                  onTap: () {
-                    //누르면 실행될 동작
-                  },
-                  child: Container(
-                    width: 320,
-                    height: 60,
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.primary,
-                      border: Border.all(
-                        width: 1.5,
-                      ),
-                      borderRadius: BorderRadius.circular(30.0),
-                    ),
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 141, vertical: 20),
-                          child: Text(
-                            "로그인",
-                            style: TextStyle(
-                              fontWeight: FontWeight.w400,
-                              color: Theme.of(context).colorScheme.onPrimary,
-                              fontSize: 14,
-                              fontFamily: 'Pretendard-bold',
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              
-                SizedBox(width: 14,),
-
-                GestureDetector(
-                  onTap: () {
-                    //누르면 실행될 동작
-                  },
-                  child: Container(
-                    width: 320,
-                    height: 60,
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.background,
-                      border: Border.all(
-                        width: 1.5,
-                      ),
-                      borderRadius: BorderRadius.circular(30.0),
-                    ),
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 141, vertical: 20),
-                          child: Text(
-                            "새 계정 생성",
-                            style: TextStyle(
-                              fontWeight: FontWeight.w400,
-                              color: Theme.of(context).colorScheme.primary,
-                              fontSize: 14,
-                              fontFamily: 'Pretendard-bold',
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-
-            
-              ],
-            ),
-          ),
-
-
-
             
           ],
         ),
---
+++ vscode_app/lib/screens/login_screen.dart
@@ -0,0 +1,323 @@
+import 'package:codi/screens/new_account_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Login());
+}
+
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Align(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(top: 82),
+              alignment: Alignment.center,
+              child: Text("로그인",
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                color: Theme.of(context).colorScheme.onBackground,
+                fontSize: 20,
+                fontFamily: 'Pretendard-Bold'),
+              ),
+            ),
+
+
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              child: Text("계정을 입력하거나 소셜 계정으로 로그인 하세요",
+                style: TextStyle(
+                fontWeight: FontWeight.w400,
+                color: Theme.of(context).colorScheme.onBackground,
+                fontSize: 14,
+                fontFamily: 'Pretendard-Bold'),
+              ),
+            ),
+            
+
+            Container(
+              margin: EdgeInsetsDirectional.only(top: 44),
+              width: 320,
+              height: 52,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Enter your Email*",
+                  hintStyle: TextStyle(
+                     color: Theme.of(context).colorScheme.onBackground.withOpacity(0.7),
+                  ),
+                  prefixIcon: Padding(
+                    padding: EdgeInsets.only(left: 22, right: 12),
+                    child: Image.asset(
+                      'assets/img/Email.png',
+                      width: 24,
+                      height: 24,
+                      color: Theme.of(context).colorScheme.onBackground.withOpacity(0.7),
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide(
+                      color: Theme.of(context).colorScheme.onBackground.withOpacity(0.7),
+                      width: 1.5,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+
+            Container(
+              margin: EdgeInsets.only(top: 14),
+              width: 320,
+              height: 52,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Password*",
+                  hintStyle: TextStyle(
+                     color: Theme.of(context).colorScheme.onBackground.withOpacity(0.7),
+                  ),
+                  prefixIcon: Padding(
+                    padding: EdgeInsets.only(left: 22, right: 12),
+                    child: Image.asset(
+                      'assets/img/Password.png',
+                      width: 24,
+                      height: 24,
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide(
+                      color: Theme.of(context).colorScheme.onBackground.withOpacity(0.7),
+                      width: 1.5,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+              
+
+            Container(
+              margin: EdgeInsets.only(top: 18),
+              width: 302,
+              height: 34,
+              child: Text(
+                "계속하기를 클릭하면 이용 약관과 개인정보 처리방침에 동의하게 됩니다.",
+                  style: TextStyle(
+                  fontWeight: FontWeight.w200,
+                  color: Theme.of(context).colorScheme.onBackground.withOpacity(0.7),
+                  fontSize: 12,
+                  fontFamily: 'Pretendard-Light'),
+              ),
+            ),
+
+            
+            Container(
+              margin: EdgeInsets.only(top: 38),
+              alignment: Alignment.center,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Divider(
+                      color: Theme.of(context).colorScheme.onBackground.withOpacity(0.7),
+                      thickness: 1,
+                      indent: 20,
+                      endIndent: 10,
+                    ),
+                  ),
+                  
+                  Text(
+                    "Or Sign In With",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w200,
+                      color: Theme.of(context).colorScheme.onBackground,
+                      fontSize: 12,
+                      fontFamily: 'Pretendard-Light',
+                    ),
+                  ),
+
+                  Expanded(
+                    child: Divider(
+                      color: Theme.of(context).colorScheme.onBackground.withOpacity(0.7),
+                      thickness: 1,
+                      indent: 10,
+                      endIndent: 20,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+
+            Container(
+              margin: EdgeInsets.only(top: 19),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 95,
+                    height: 52,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Theme.of(context).colorScheme.onBackground.withOpacity(0.7),
+                        width: 1.5,
+                      ),
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    child: GestureDetector(
+                    onTap: () {
+                      //누르면 실행될 동작
+                    },
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                          child: Image.asset(
+                            'assets/img/google.png',
+                            width: 32,
+                            height: 32,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                
+                  Container(
+                    margin: EdgeInsets.only(left: 17),
+                    width: 95,
+                    height: 52,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Theme.of(context).colorScheme.onBackground.withOpacity(0.7),
+                        width: 1.5,
+                      ),
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    child: GestureDetector(
+                    onTap: () {
+                      //누르면 실행될 동작
+                    },
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                          child: Image.asset(
+                            'assets/img/facebook.png',
+                            width: 32,
+                            height: 32,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  
+                  Container(
+                    margin: EdgeInsets.only(left: 17),
+                    width: 95,
+                    height: 52,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Theme.of(context).colorScheme.onBackground.withOpacity(0.7),
+                        width: 1.5,
+                      ),
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    child: GestureDetector(
+                    onTap: () {
+                      //누르면 실행될 동작
+                    },
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                          child: Image.asset(
+                            'assets/img/kakao_talk.png',
+                            width: 32,
+                            height: 32,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+
+            Container(
+              margin: EdgeInsets.only(top: 154),
+              width: 320,
+              height: 60,
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+                border: Border.all(
+                  width: 1.5,
+                ),
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              child: GestureDetector(
+                onTap: () {
+                  //누르면 실행될 동작
+                },
+                child: Center(
+                  child: Text(
+                    "로그인",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      color: Theme.of(context).colorScheme.onPrimary,
+                      fontSize: 14,
+                      fontFamily: 'Pretendard-bold',
+                    ),
+                  ),
+                )
+              ),
+            ),
+                
+            Container(
+              margin: EdgeInsets.only(top: 14),
+              width: 320,
+              height: 60,
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.background,
+                border: Border.all(
+                  width: 1.5,
+                ),
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => NewAccountScreen(),
+                    ),
+                  );
+                },
+                child: Center(
+                  child: Text(
+                    "새 계정 생성",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      color: Theme.of(context).colorScheme.primary,
+                      fontSize: 14,
+                      fontFamily: 'Pretendard-bold',
+                    ),
+                  ),
+                )
+              ),
+            ),
+           
+          ],
+        ),
+      ),
+    );
+  }
+}
